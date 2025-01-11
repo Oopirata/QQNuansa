5f5a1808802d480463e8ff076a8e5f6631702fb1
ixariis <suryafajar04@gmail.com>
2025-01-11 13:02:51 +0700

    edit app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link href="https://fonts.googleapis.com/css2?family=Kaisei+Opti&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&amp;display=swap" rel="stylesheet"/>
         
         <!-- Font Awesome -->
         <link rel="preconnect" href="https://cdnjs.cloudflare.com">
