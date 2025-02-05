ae344d56bb1406089be33bcbca115c0e8f745534
Oopirata <herovva@gmail.com>
2025-02-05 21:50:59 +0700

    Refactor AdminEmail.vue to integrate Sidebar component and streamline layout

--- resources/js/Pages/AdminInterviewCandidates.vue
+++ resources/js/Pages/AdminInterviewCandidates.vue
@@ -2,6 +2,7 @@
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { Head, Link } from "@inertiajs/vue3";
+import Sidebar from "@/Components/Sidebar/Sidebar.vue";
 
 const dropdownOpen = ref(false);
 const dropdownRef = ref(null);
@@ -82,134 +83,7 @@ const toggleSubMenu = () => {
 <template>
     <Head title="Candidates Dashboard" />
     <div class="flex h-screen">
-        <!-- Sidebar -->
-        <div class="bg-blue-100 w-64 p-4 flex flex-col">
-            <div class="flex items-center mb-8">
-                <img
-                    src="/images/QQ crop.png"
-                    alt="Company Logo"
-                    class="w-20 h-16 mr-2"
-                />
-                <img
-                    src="/images/Nuansa crop.png"
-                    alt="Company Logo"
-                    class="w-24 h-8 mr-2 ml-3"
-                />
-            </div>
-
-            <nav class="flex-1">
-                <ul>
-                    <li class="mb-4">
-                        <Link
-                            href="/adminDashboard"
-                            class="flex items-center text-gray-600 hover:text-gray-900"
-                        >
-                            <i class="fas fa-tachometer-alt mr-2"></i>
-                            Dashboard
-                        </Link>
-                    </li>
-                    <li class="mb-4">
-                        <button
-                            @click="toggleSubMenu"
-                            class="flex items-center w-full text-gray-600 hover:text-gray-900"
-                        >
-                            <i class="fas fa-users mr-2"></i>
-                            Candidates
-                            <i
-                                :class="[
-                                    'fas ml-auto',
-                                    isSubMenuOpen
-                                        ? 'fa-chevron-down'
-                                        : 'fa-chevron-right',
-                                ]"
-                            ></i>
-                        </button>
-                        <ul v-if="isSubMenuOpen" class="ml-6 mt-2">
-                            <li
-                                v-for="item in candidateItems"
-                                :key="item.name"
-                                class="mb-2"
-                            >
-                                <Link
-                                    :href="item.path"
-                                    :class="{
-                                        'text-purple-600':
-                                            item.name === 'Interview', // Menjadi ungu jika item bernama New
-                                        'text-gray-600 hover:text-gray-900':
-                                            item.name !== 'Interview', // Tetap abu-abu untuk yang lain
-                                    }"
-                                >
-                                    {{ item.name }}
-                                </Link>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="mb-4">
-                        <Link
-                            href="/adminEmail"
-                            class="flex items-center text-gray-600 hover:text-gray-900"
-                        >
-                            <i class="fas fa-envelope mr-2"></i>
-                            E-mail
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <template v-if="$page.props.auth.user">
-                <div class="mt-auto relative" ref="dropdownRef">
-                    <button
-                        @click="toggleDropdown"
-                        type="button"
-                        class="flex items-center w-full p-2 hover:bg-gray-100 rounded-md">
-                            <img
-                                src="/images/profile.png"
-                                alt="User Avatar"
-                                class="w-10 h-10 rounded-full mr-2"
-                            />
-                            <div>
-                                <p class="text-gray-600">
-                                    {{ $page.props.auth.user.name }}
-                                </p>
-                            </div>
-                    </button>
-                    
-                    <Transition
-                        enter-active-class="transition ease-out duration-100"
-                        enter-from-class="transform opacity-0 translate-y-2"
-                        enter-to-class="transform opacity-100 translate-y-0"
-                        leave-active-class="transition ease-in duration-75"
-                        leave-from-class="transform opacity-100 translate-y-0"
-                        leave-to-class="transform opacity-0 translate-y-2"
-                    >
-                        <div
-                            v-show="dropdownOpen"
-                            class="absolute left-0 bottom-full mb-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 py-1 z-50"
-                        >
-                            <Link
-                                :href="route('profile.edit')"
-                                class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                            >
-                                Profile
-                            </Link>
-                            <Link
-                                v-if="$page.props.auth.user.roles.some((role) => role.role_name === 'admin')"
-                                :href="route('landing.page')"
-                                class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                            >
-                                Website
-                            </Link>
-                            <Link
-                                :href="route('logout')"
-                                method="post"
-                                class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                            >
-                                Log Out
-                            </Link>
-                        </div>
-                    </Transition>
-                </div>
-            </template>
-        </div>
+        <Sidebar :user="page.props.auth.user" />
 
         <!-- Main Content -->
         <div class="flex-1 p-6">
