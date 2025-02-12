01b9f9da0a252e4a807c5a047f598b80d8aed204
Oopirata <herovva@gmail.com>
2025-02-12 10:46:06 +0700

    Add CV upload functionality and enhance Applicant model with additional fields

--- database/migrations/2025_01_15_133127_create_applicants_table.php
+++ database/migrations/2025_01_15_133127_create_applicants_table.php
@@ -12,8 +12,16 @@ return new class extends Migration
             $table->id();
             $table->foreignId('users_id')->constrained()->onDelete('cascade');
             $table->foreignId('job_vacancy_id')->constrained()->onDelete('cascade');
+            $table->string('name');
             $table->string('degree');
-            $table->string('expected_salary');
+            $table->decimal('ipk', 3, 2);
+            $table->string('nomor_hp');
+            $table->string('province');
+            $table->string('city');
+            $table->integer('expected_salary');
+            $table->string('applicant_email');
+            $table->string('linkedin')->nullable();
+            $table->string('cv_path');
             $table->integer('status')->default(0);
             $table->timestamps();
         });
